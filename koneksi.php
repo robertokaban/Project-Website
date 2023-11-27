ab8df8e4e08c8eb1969caadb2f9e2d8739b2e3b9
Roberto Kaban <roberto.kaban@yahoo.com>
2023-11-27 16:23:59 +0700

    edit koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$koneksi= new mysqli("localhost","root","ruth","akademik");
+$koneksi= new mysqli("localhost","root","","akademik");
 
 //$koneksi= new mysqli("HOST_ATAU_IP_SERVE","USERNAME_AKSES_MYSQL","PASSWORD_MYSQL","NAMA_DATABASE");
 
